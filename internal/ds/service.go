ba9b30953e87fc4a4cdf5249ab7e1a0156179cc9
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2020-12-30 09:38:54 +0200

    Added debug schedule

--- internal/ds/service.go
+++ internal/ds/service.go
@@ -3,6 +3,8 @@ package ds
 import (
 	"strings"
 	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 type Service struct {
@@ -30,12 +32,22 @@ type DaySchedule struct {
 
 const daysToSchedule = 7
 
-var Schedule map[time.Weekday]DaySchedule
+var Schedule = map[time.Weekday]DaySchedule{
+	time.Monday: {
+		StartHour:    17,
+		StartMin:     0,
+		EndHour:      20,
+		EndMin:       0,
+		SlotDuration: 30,
+	},
+}
 
 func (s *Service) SyncSlots(startTime time.Time) error {
+	logrus.Infof("Started syncing slots with start time %s", startTime.Format(time.RFC3339))
 	startDate := time.Date(startTime.Year(), startTime.Month(), startTime.Day(), 0, 0, 0, 0, time.UTC)
 
 	freeSlots, err := s.portalClient.GetFreeSlots(startDate, daysToSchedule)
+	logrus.Debugf("found free slots %v", freeSlots)
 	if err != nil {
 		return err
 	}
@@ -49,6 +61,7 @@ func (s *Service) SyncSlots(startTime time.Time) error {
 			continue
 		}
 		freeDaySlots := getFreeDaySlots(freeSlots, dayToProcess)
+		logrus.Debugf("found free slots for day %v : %v", dayToProcess, freeDaySlots)
 		if freeDaySlots == nil {
 			continue
 		}
@@ -58,6 +71,7 @@ func (s *Service) SyncSlots(startTime time.Time) error {
 		}
 	}
 
+	logrus.Info("Finished syncing slots")
 	return nil
 }
 
@@ -67,6 +81,7 @@ func (s *Service) syncDay(day time.Time, sched DaySchedule, slots *FreeDaySlots)
 
 	for slotTime.Before(endTime) {
 		slotIsFree := isSlotFree(slotTime, slots)
+		logrus.Debugf("slot at time %v is free %v", slotTime, slotIsFree)
 		if !slotIsFree {
 			if err := s.cal.CreateAppointment(slotTime, sched.SlotDuration); err != nil {
 				return err
